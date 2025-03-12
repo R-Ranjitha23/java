603c9ef2acc2db89c9e113ef0326e895dbc0e467
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-12 15:23:42 +0530

    add

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class concat {
+    public static void main(String[]args){
+        String name1="Ranjitha";
+        String name2="Ramachandran";
+             System.out.println(name1.concat(name2));
+    }
+    
+}
