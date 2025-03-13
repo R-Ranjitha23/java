9a4f4b41c3ac577006206c3119633f23bbe92492
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 10:23:51 +0530

    add

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+        int mark=10;
+        double decimal=mark;
+        System.out.println("Enter the value:"+decimal);
+
+    }
+    
+}
