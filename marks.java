dccd13eaec10478253aa89b404fa0b8f76c529cb
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 15:21:12 +0530

    add

---
+++ marks.java
@@ -0,0 +1,13 @@
+public class marks {
+    public static void main(String[] args) {
+        int mark = 65;
+        
+        if (mark >= 50) { 
+            System.out.println("Grade B");
+        } else if (mark < 50) { 
+            System.out.println("Grade A");
+        } else {
+            System.out.println("Grade C"); 
+        }
+    }
+}
