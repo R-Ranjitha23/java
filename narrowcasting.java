da5e4ee935ac5aaf7a77dc69faaebb56e1357f79
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 10:43:34 +0530

    add

---
+++ narrowcasting.java
@@ -0,0 +1,12 @@
+public class narrowcasting {
+        public static void main(String[] args) {
+           double value=2.3d;
+           int number=(int)value;
+            System.out.println("Enter the value:"+number);
+    
+        }
+        
+    }
+    
+    
+
