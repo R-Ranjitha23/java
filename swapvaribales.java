b42af53428d5ac982b9e2437c4be60999e3897ac
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 13:31:24 +0530

    add

---
+++ swapvaribales.java
@@ -0,0 +1,14 @@
+public class swapvaribales {
+    public static void main(String[] args) {
+        int a=9;
+        int b=6;  
+                a = a + b; 
+                b = a - b;
+                a = a - b; 
+        
+                System.out.println("After swapping: a = " + a + ", b = " + b);
+            }
+        }
+    
+    
+
