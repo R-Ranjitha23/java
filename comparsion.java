72969b850a67690590ccdfa9bf385edec8e7c38e
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 11:57:13 +0530

    add

---
+++ comparsion.java
@@ -0,0 +1,9 @@
+public class comparsion {
+    public static void main(String[] args) {
+        int value1=212205;
+        int value2=235205;
+        System.out.println(value1==value2);
+        System.out.println(value1!=value2);
+        System.out.println(value1>=value2);
+    }
+}
