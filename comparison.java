0c14607f2f495856b60a69c3a46106f5b17625e7
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 11:48:59 +0530

    add

---
+++ comparison.java
@@ -0,0 +1,7 @@
+public class comparison {
+    public static void main(String[] args) {
+        String name1="ranjitha";
+        String name2="ranjith";
+        System.out.println(name1==(name2));
+    }
+}
