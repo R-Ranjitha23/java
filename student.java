0ca25c93ab621a76c22572c9aaf5278dc375c1ee
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 09:51:29 +0530

    add

---
+++ student.java
@@ -0,0 +1,12 @@
+public class student {
+    public static void main(String[]args){
+        String name="Ranjitha";
+        int mark=532;
+        char grade = 'o';
+        String subject="physiscs";
+        System.out.println(name);
+        System.out.println(mark);
+        System.out.println(grade);
+        System.out.println(subject);
+    }
+}
