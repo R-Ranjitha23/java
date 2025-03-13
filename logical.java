c48531ed36e03cdf8a150115310c026c8d70f664
R-Ranjitha23 <pramachandaran79@gmail.com>
2025-03-13 12:10:20 +0530

    add

---
+++ logical.java
@@ -0,0 +1,9 @@
+public class logical {
+    public static void main(String[] args) {
+        int a=212205;
+        int b=235205;
+         boolean values = (a==b && a>b);
+        System.out.println(values);
+    }
+    
+}
